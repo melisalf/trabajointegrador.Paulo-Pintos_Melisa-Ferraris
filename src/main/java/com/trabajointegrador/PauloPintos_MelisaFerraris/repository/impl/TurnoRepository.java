ad07f3f5e99225fd2807f5019209fee56e33ba71
GodSavitar19 <106354483+GodSavitar19@users.noreply.github.com>
2023-06-15 23:03:00 -0300

    Update

--- src/main/java/com/trabajointegrador/PauloPintos_MelisaFerraris/repository/impl/TurnoRepository.java
+++ src/main/java/com/trabajointegrador/PauloPintos_MelisaFerraris/repository/impl/TurnoRepository.java
@@ -5,6 +5,9 @@ package com.trabajointegrador.PauloPintos_MelisaFerraris.repository.impl;
 import com.trabajointegrador.PauloPintos_MelisaFerraris.entity.Turno;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
 
 public interface TurnoRepository extends JpaRepository<Turno, Long> {
 
